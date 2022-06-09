1565bbd556205aef7372e1d6089db366e8c630f7
DrMaxPower <ni.fe@icloud.com>
2022-06-09 21:39:20 +0000

    terminal design almost done

--- run.py
+++ run.py
@@ -26,22 +26,26 @@ H = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
 
 def board():
     """"""
+    player_score = 7
+    computer_score = 4
+    tunrs = 3
 
     wall = '|'
     roof = '-=-'
     mine = '¤ '
-    fence = '# '
-    print("NAME")
+
+    print(" "*20)
+    print("PLAYER", " "*3, f"SCORE: {player_score}")
     print(' ', '  1', '  2',  '  3', '  4')
     print('+', roof*5, '+')
     print('A', wall, A[1], A[2], A[3], A[4], wall)
-    print('B', wall, B[1], B[2], B[3], B[4],  wall)
-    print('C', wall, C[1], C[2], C[3], C[4],  wall)
-    print('D', wall, D[1], D[2], D[3], D[4],  wall)
+    print('B', wall, B[1], B[2], B[3], B[4], wall)
+    print('C', wall, C[1], C[2], C[3], C[4], wall)
+    print('D', wall, D[1], D[2], D[3], D[4], wall)
 
-    print(mine*10)
-    print(fence*10)
-    print(mine*10)
+    print('  +', mine*7, '+')
+    print(' ', wall, f"Turns left: {tunrs}", wall)
+    print('  +', mine*6, '+')
 
     print('E', wall, E[1], E[2], E[3], E[4], wall)
     print('F', wall, F[1], F[2], F[3], F[4],  wall)
@@ -49,7 +53,8 @@ def board():
     print('H', wall, H[1], H[2], H[3], H[4],  wall)
     print('+', roof*5, '+')
     print(' ', '  1', '  2',  '  3', '  4')
-    print("COMPUTER")
+    print("COMPUTER", " ", f"SCORE: {computer_score}")
+    print(" "*20)
 
 
 board()
