793329c7a11e7a7bee90af9e6b19f1d94a46b57c
DrMaxPower <ni.fe@icloud.com>
2022-06-09 16:46:37 +0000

    init setting up relevant functions

--- run.py
+++ run.py
@@ -1,1 +1,55 @@
-# python code goes here
+import random
+
+# all input data needs to be in a new row
+# on Heroku: key = PORT, value = 8000
+
+
+def get_input_user():
+    """ ship position and shoot at enemy position """
+    pass
+
+
+def validate_user_input():
+    """  """
+    pass
+
+
+def get_comuter_data():
+    """  """
+    pass
+
+
+def board():
+    """  """
+    pass
+
+
+def hit_target():
+    """ How many sunken ships """
+    pass
+
+
+def main():
+    """ the game """
+    pass
+
+
+# print("welcome")
+# main()
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+print("welcome")
