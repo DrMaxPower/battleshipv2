e72449102620003925ec395fdeca69bd94100681
DrMaxPower <ni.fe@icloud.com>
2022-06-10 10:36:18 +0000

    change to function form

--- run.py
+++ run.py
@@ -1,53 +1,73 @@
 """ BATTLEFIELD """
 import random
-from collections import Counter
-
 # all input data needs to be in a new row
 # on Heroku: key = PORT, value = 8000
 
 
-
-# Players Board
-p = {
-    'A': {1: 'x', 2: '*', 3: '*', 4: '*'},
-    'B': {1: '*', 2: '*', 3: 'x', 4: '*'},
-    'C': {1: '*', 2: '*', 3: '*', 4: '*'},
-    'D': {1: 'x', 2: 'o', 3: '*', 4: '*'}
-
-}
+def input_player():
+    pass
 
 
-computer_score = 0
-for i in range(4):
-    letter_player = chr(65+i)
-    hit_player = (p.get(letter_player).values())
-    for item in hit_player:
-        if item == 'x':
-            computer_score += 1
+def validate_input_player():
+    pass
 
 
-# Computers Board
-c = {
-    'E': {1: 'x', 2: '*', 3: '*', 4: '*'},
-    'F': {1: '*', 2: '*', 3: '*', 4: '*'},
-    'G': {1: '*', 2: 'x', 3: '*', 4: '*'},
-    'H': {1: '*', 2: '*', 3: 'x', 4: '*'}
-}
+def place_ship_computer():
+    pass
+
+def place_ship_player():
+    pass
 
 
-player_score = 0
-for j in range(4):
-    letter_computer = chr(69+j)
-    hit_computer = (c.get(letter_computer).values())
-    for item in hit_computer:
-        if item == 'x':
-            player_score += 1
-
-
-
+def player_shoot():
+    pass
 
 
 def board():
+    """ player and compter board """
+
+    # Players Board
+    p = {
+        'A': {1: 'x', 2: '*', 3: '*', 4: '*'},
+        'B': {1: '*', 2: '*', 3: 'x', 4: '*'},
+        'C': {1: '*', 2: '*', 3: '*', 4: '*'},
+        'D': {1: 'x', 2: 'o', 3: '*', 4: '*'}
+    }
+
+    # Computers Board
+    c = {
+        'E': {1: 'x', 2: '*', 3: '*', 4: '*'},
+        'F': {1: '*', 2: '*', 3: '*', 4: '*'},
+        'G': {1: '*', 2: 'x', 3: '*', 4: '*'},
+        'H': {1: '*', 2: '*', 3: 'x', 4: '*'}
+    }
+
+    return p, c
+
+
+def score():
+    """ seting score for player and computer """
+
+    player_score = 0
+    for j in range(4):
+        letter_computer = chr(69+j)
+        hit_computer = (c.get(letter_computer).values())
+        for item in hit_computer:
+            if item == 'x':
+                player_score += 1
+
+    computer_score = 0
+    for i in range(4):
+        letter_player = chr(65+i)
+        hit_player = (p.get(letter_player).values())
+        for item in hit_player:
+            if item == 'x':
+                computer_score += 1
+
+    return player_score, computer_score
+
+
+def board_ternminal(p, c, player_score, computer_score):
     """"""
     tunrs = 3
 
@@ -78,5 +98,5 @@ def board():
     print(" "*20)
 
 
-board()
-
+def main():
+    pass
