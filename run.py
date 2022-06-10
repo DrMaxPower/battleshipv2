a4c2b98193b37df92818556b25d4a49b3962b39d
DrMaxPower <ni.fe@icloud.com>
2022-06-10 10:05:48 +0000

    score variable logic done, tryed Counter() but with not trivial method

--- run.py
+++ run.py
@@ -1,33 +1,54 @@
 """ BATTLEFIELD """
 import random
+from collections import Counter
 
 # all input data needs to be in a new row
 # on Heroku: key = PORT, value = 8000
 
-# NAME = input("What is your GamePlay Name_:")
-
-
 
 
 # Players Board
-A = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
-B = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
-C = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
-D = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
+p = {
+    'A': {1: 'x', 2: '*', 3: '*', 4: '*'},
+    'B': {1: '*', 2: '*', 3: 'x', 4: '*'},
+    'C': {1: '*', 2: '*', 3: '*', 4: '*'},
+    'D': {1: 'x', 2: 'o', 3: '*', 4: '*'}
 
+}
+
+
+computer_score = 0
+for i in range(4):
+    letter_player = chr(65+i)
+    hit_player = (p.get(letter_player).values())
+    for item in hit_player:
+        if item == 'x':
+            computer_score += 1
 
 
 # Computers Board
-E = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
-F = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
-G = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
-H = {1: '* ', 2: ' * ', 3: ' * ', 4: ' *'}
+c = {
+    'E': {1: 'x', 2: '*', 3: '*', 4: '*'},
+    'F': {1: '*', 2: '*', 3: '*', 4: '*'},
+    'G': {1: '*', 2: 'x', 3: '*', 4: '*'},
+    'H': {1: '*', 2: '*', 3: 'x', 4: '*'}
+}
+
+
+player_score = 0
+for j in range(4):
+    letter_computer = chr(69+j)
+    hit_computer = (c.get(letter_computer).values())
+    for item in hit_computer:
+        if item == 'x':
+            player_score += 1
+
+
+
 
 
 def board():
     """"""
-    player_score = 7
-    computer_score = 4
     tunrs = 3
 
     wall = '|'
@@ -36,25 +57,26 @@ def board():
 
     print(" "*20)
     print("PLAYER", " "*3, f"SCORE: {player_score}")
-    print(' ', '  1', '  2',  '  3', '  4')
-    print('+', roof*5, '+')
-    print('A', wall, A[1], A[2], A[3], A[4], wall)
-    print('B', wall, B[1], B[2], B[3], B[4], wall)
-    print('C', wall, C[1], C[2], C[3], C[4], wall)
-    print('D', wall, D[1], D[2], D[3], D[4], wall)
+    print('   ', '1', '2', '3', '4')
+    print('+', roof*3, '+')
+    print('A', wall, p['A'][1], p['A'][2], p['A'][3], p['A'][4], wall, " * : water ")
+    print('B', wall, p['B'][1], p['B'][2], p['B'][3], p['B'][4], wall, " o : ship")
+    print('C', wall, p['C'][1], p['C'][2], p['C'][3], p['C'][4], wall, " x : hit")
+    print('D', wall, p['D'][1], p['D'][2], p['D'][3], p['D'][4], wall)
 
-    print('  +', mine*7, '+')
-    print(' ', wall, f"Turns left: {tunrs}", wall)
-    print('  +', mine*6, '+')
+    print('  +', mine*4)
+    print(f"  Turn: {tunrs}")
+    print('  +', mine*4)
 
-    print('E', wall, E[1], E[2], E[3], E[4], wall)
-    print('F', wall, F[1], F[2], F[3], F[4],  wall)
-    print('G', wall, G[1], G[2], G[3], G[4],  wall)
-    print('H', wall, H[1], H[2], H[3], H[4],  wall)
-    print('+', roof*5, '+')
-    print(' ', '  1', '  2',  '  3', '  4')
+    print('E', wall, c['E'][1], c['E'][2], c['E'][3], c['E'][4], wall, " 1 : Position your ship")
+    print('F', wall, c['F'][1], c['F'][2], c['F'][3], c['F'][4], wall, " 2 : Enter your shoot")
+    print('G', wall, c['G'][1], c['G'][2], c['G'][3], c['G'][4], wall)
+    print('H', wall, c['H'][1], c['H'][2], c['H'][3], c['H'][4], wall)
+    print('+', roof*3, '+')
+    print('  ', '1', '2',  '3', '4')
     print("COMPUTER", " ", f"SCORE: {computer_score}")
     print(" "*20)
 
 
 board()
+
